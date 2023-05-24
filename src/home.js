8c7fe366a4647e9cd2d095cedc831e5aba94b4e1
rib9985 <rib9985@gmail.com>
2023-05-24 11:28:33 -0300

    Create text.js file to import and export text

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import aboutParagraph from "./text"
+
 function createParagraph(text, cssClassName){
     const paragraph = document.createElement('p')
     paragraph.textContent= text
@@ -23,10 +25,9 @@ function createHeader(typeOfHeader, text,cssClassName){
 
 function createHome(){
     const home = document.createElement('div')
-    home.className = 'home'
-
+    home.setAttribute('class', 'home')
     home.appendChild(createHeader('h1','Who Are We?','home-header'))
-    home.appendChild(createParagraph())
+    home.appendChild(createParagraph(aboutParagraph,'home-paragraph'))
 
 }
 
---
+++ src/text.js
@@ -0,0 +1,5 @@
+const aboutParagraph = 'This is story about how Italy and its cuisine was made. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed diam euismod, vulputate ante nec, euismod diam. Vivamus bibendum libero lorem, id sodales nunc auctor nec.'
+const moreAboutUs = 'Check out more about us on this website! Feel free to navigate! Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+
+
+export default aboutParagraph; moreAboutUs
