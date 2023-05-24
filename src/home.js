1393b5b82d8c634c38a5208150317d23bdce5374
rib9985 <rib9985@gmail.com>
2023-05-24 11:21:39 -0300

    Create functions and modules for home page

---
+++ src/home.js
@@ -0,0 +1,38 @@
+function createParagraph(text, cssClassName){
+    const paragraph = document.createElement('p')
+    paragraph.textContent= text
+    paragraph.setAttribute('class', `${cssClassName}`)
+return paragraph
+}
+
+function createImage(imageSource, alt, cssClassName){
+    const image = document.createElement('img')
+    image.alt= alt
+    image.src=imageSource
+    image.setAttribute('class', `${cssClassName}`)
+return image
+}
+
+
+function createHeader(typeOfHeader, text,cssClassName){
+    const header = document.createElement(`${typeOfHeader}`)
+    header.textContent = text
+    header.setAttribute('class',`${cssClassName}`)
+    return header
+}
+
+function createHome(){
+    const home = document.createElement('div')
+    home.className = 'home'
+
+    home.appendChild(createHeader('h1','Who Are We?','home-header'))
+    home.appendChild(createParagraph())
+
+}
+
+
+
+
+function loadHome(){
+
+}
